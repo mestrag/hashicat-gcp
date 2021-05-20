47c97c70b583391e6886de4346478be189a29c41
mestrag <raghu.mesta26@gmail.com>
2021-05-20 17:39:40 +0000

    Corrected the vpc network

--- vpc.tf
+++ vpc.tf
@@ -2,11 +2,11 @@ module "network" {
   source  = "terraform-google-modules/network/google"
   version = "3.2.2"
   # insert the 3 required variables here
-  network_name = "raghu-network"
+  network_name = raghu26-network
   project_id = var.project
   subnets = [
   {
-    subnet_name   = "raghu-subnet"
+    subnet_name   = "raghu26-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
